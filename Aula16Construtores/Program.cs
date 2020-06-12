b0b47387cd09f04a024ac5de0f6f8bed060db0c5
Paulo Brandão <paulobrandaoofficial@gmail.com>
2020-06-12 16:26:02 -0300

    Aula16 finalizada

---
+++ Aula16Construtores/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula16Construtores
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instância sem argumentos
+            Produto sapato = new Produto();
+            sapato.Nome = "Vizzano";
+
+            // Instância com 1 argumento
+            Produto chinelo = new Produto(45);
+            System.Console.WriteLine(chinelo.Codigo);
+
+            // Instância com todos os atributos como argumento
+            Produto camisa = new Produto(34, "Supreme", "Azul", 234);
+            System.Console.WriteLine(camisa.Codigo);
+            System.Console.WriteLine(camisa.Nome);
+            System.Console.WriteLine(camisa.Descricao);
+            System.Console.WriteLine(camisa.Estoque);
+
+        }
+    }
+}
